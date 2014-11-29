83dc9dd5633fe431e5eeaf541437483ad1ee464a
Hristo Lungov <itseto@abv.bg>
2014-11-29 09:05:27 +0200

    Added first trys to override surf beans.

--- dms-sys-parent/dms-sys-repo-api-impl/src/main/java/org/dms/sys/semantic/db/SemanticDataSource.java
+++ dms-sys-parent/dms-sys-repo-api-impl/src/main/java/org/dms/sys/semantic/db/SemanticDataSource.java
@@ -16,63 +16,66 @@ public class SemanticDataSource {
 
 	/** The base uri. */
 	private final String BASE_URI = "openrdf-sesame";
-	
+
 	/** The repo uri. */
 	private final String REPO_URI = "repositories";
-	
+
 	/** The server. */
 	private String server;
-	
+
 	/** The name. */
 	private String name;
-	
+
 	/** The port. */
 	private int port;
-	
+
 	/** The url. */
 	private String protocol;
-	
+
 	/** The password. */
 	private String password;
-	
+
 	/** The username. */
 	private String username;
-	
+
 	/** The manager. */
 	private RemoteRepositoryManager manager;
-	
+
 	/** The repository. */
 	private Repository repository;
-	
+
 	/** The repository connection. */
 	public RepositoryConnection repositoryConnection;
-	
+
 	/**
 	 * Inits the.
 	 */
 	void init() {
-		String serverUrl = protocol + "://" + server + ":" + port + "/" + BASE_URI;
+		String serverUrl = protocol + "://" + server + ":" + port + "/"
+				+ BASE_URI;
 		manager = new RemoteRepositoryManager(serverUrl);
 		try {
-			manager.initialize();
-			repository = manager.getRepository(name);
-			repositoryConnection = repository.getConnection();
+			if (manager.isInitialized()) {
+				manager.initialize();
+				repository = manager.getRepository(name);
+				repositoryConnection = repository.getConnection();
+			}
 		} catch (RepositoryException e) {
 			e.printStackTrace();
 		} catch (RepositoryConfigException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Gets the repository connection.
-	 *
+	 * 
 	 * @return the repository connection
 	 */
 	public RepositoryConnection getRepositoryConnection() {
 		return repositoryConnection;
 	}
-	
+
 	/**
 	 * Close.
 	 */
@@ -80,12 +83,12 @@ public class SemanticDataSource {
 		if (manager != null && manager.isInitialized()) {
 			manager.shutDown();
 		}
-		
+
 	}
 
 	/**
 	 * Gets the name.
-	 *
+	 * 
 	 * @return the name
 	 */
 	public String getName() {
@@ -94,8 +97,9 @@ public class SemanticDataSource {
 
 	/**
 	 * Sets the name.
-	 *
-	 * @param name the new name
+	 * 
+	 * @param name
+	 *            the new name
 	 */
 	public void setName(String name) {
 		this.name = name;
@@ -103,8 +107,9 @@ public class SemanticDataSource {
 
 	/**
 	 * Sets the port.
-	 *
-	 * @param port the new port
+	 * 
+	 * @param port
+	 *            the new port
 	 */
 	public void setPort(String port) {
 		this.port = Integer.valueOf(port);
@@ -112,7 +117,7 @@ public class SemanticDataSource {
 
 	/**
 	 * Gets the server.
-	 *
+	 * 
 	 * @return the server
 	 */
 	public String getServer() {
@@ -121,8 +126,9 @@ public class SemanticDataSource {
 
 	/**
 	 * Sets the server.
-	 *
-	 * @param server the new server
+	 * 
+	 * @param server
+	 *            the new server
 	 */
 	public void setServer(String server) {
 		this.server = server;
@@ -130,7 +136,7 @@ public class SemanticDataSource {
 
 	/**
 	 * Gets the protocol.
-	 *
+	 * 
 	 * @return the protocol
 	 */
 	public String getProtocol() {
@@ -139,8 +145,9 @@ public class SemanticDataSource {
 
 	/**
 	 * Sets the protocol.
-	 *
-	 * @param protocol the new protocol
+	 * 
+	 * @param protocol
+	 *            the new protocol
 	 */
 	public void setProtocol(String protocol) {
 		this.protocol = protocol;
@@ -148,7 +155,7 @@ public class SemanticDataSource {
 
 	/**
 	 * Gets the password.
-	 *
+	 * 
 	 * @return the password
 	 */
 	public String getPassword() {
@@ -157,8 +164,9 @@ public class SemanticDataSource {
 
 	/**
 	 * Sets the password.
-	 *
-	 * @param password the new password
+	 * 
+	 * @param password
+	 *            the new password
 	 */
 	public void setPassword(String password) {
 		this.password = password;
@@ -166,7 +174,7 @@ public class SemanticDataSource {
 
 	/**
 	 * Gets the username.
-	 *
+	 * 
 	 * @return the username
 	 */
 	public String getUsername() {
@@ -175,8 +183,9 @@ public class SemanticDataSource {
 
 	/**
 	 * Sets the username.
-	 *
-	 * @param username the new username
+	 * 
+	 * @param username
+	 *            the new username
 	 */
 	public void setUsername(String username) {
 		this.username = username;
